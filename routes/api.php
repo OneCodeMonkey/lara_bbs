c632dc5b0f3d53e9006a10076242dd994b4ed9c4
OneCodeMonkey <1460018362@qq.com>
2019-02-25 16:32:56 +0000

    add api configuration: GET lara_bbs.test/api/version.

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,16 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function ($api) {
+	$api->get('version', function () {
+		return response('this is version v1');
+	});
+});
+
+$api->version('v2', function ($api) {
+	$api->get('version', function () {
+		return response('this is version v2');
+	});
 });
