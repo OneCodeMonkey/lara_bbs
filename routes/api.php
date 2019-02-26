779ff468b5b4d0f44984c966d85d69948c6dc2e2
OneCodeMonkey <1460018362@qq.com>
2019-02-26 15:53:31 +0000

    add image verification code step.

--- routes/api.php
+++ routes/api.php
@@ -39,5 +39,7 @@ $api->version('v1', [
 		$api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
 		// 用户注册
 		$api->post('users', 'UsersController@store')->name('api.users.store');
+		// 图片验证码
+		$api->post('captchas', 'CaptchasController@store')->name('api.captchas.store');
 	});
 });
